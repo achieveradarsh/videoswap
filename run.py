4ba1522960aa5aca35b0efbaa050d0c375bbd715
adarsh342 <126852443+adarsh342@users.noreply.github.com>
2024-09-09 12:57:43 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 # run.py
 
-import os
 from face_swapper import download_model, process_video_with_source_image
+from google.colab import files
 
 def main():
-    # User input for image and video paths
-    source_img_path = input("Enter the path for the source image: ")
-    target_video_path = input("Enter the path for the target video: ")
-    output_video_path = input("Enter the path for saving the output video: ")
+    # Upload source image
+    print("Upload the source image file")
+    uploaded = files.upload()
+    source_img_path = list(uploaded.keys())[0]
+    
+    # Upload target video
+    print("Upload the target video file")
+    uploaded = files.upload()
+    target_video_path = list(uploaded.keys())[0]
+
+    # Define output video path
+    output_video_path = "/content/result_video.mp4"
 
     # Download model from Hugging Face if not already downloaded
     model_url = "https://github.com/facefusion/facefusion-assets/releases/download/models/inswapper_128_fp16.onnx"
@@ -15,6 +23,8 @@ def main():
 
     # Process the video with the source image
     process_video_with_source_image(source_img_path, target_video_path, model_path, output_video_path)
+    
+    print(f'Video processing complete. The result video is saved at: {output_video_path}')
 
 if __name__ == "__main__":
     main()
