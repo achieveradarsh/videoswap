d64caa600549497f66bf1deab1214e40efc16bed
achieveradarsh <158289007+achieveradarsh@users.noreply.github.com>
2025-05-13 09:18:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,17 @@
-# run.py
-
 import os
-from face_swapper import process_video_with_source_image, download_model
+from face_swapper import process_image_with_source_image, download_model
 
 def main():
-    # Get file paths from user input
     source_img_path = input("Enter the path to the source image: ")
-    target_video_path = input("Enter the path to the target video: ")
+    target_img_path = input("Enter the path to the target image: ")
+    output_image_path = "output_image.jpg"
 
-    # Define a standard output path
-    output_video_path = "output_video.mp4"
-
-    # Define the model URL
     model_url = "https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx"
-    model_path = download_model(model_url, "./checkpoints/model.onnx")
+    model_path = download_model(model_url, "./checkpoints/inswapper_128.onnx")
 
-    # Run the face swapping process
-    process_video_with_source_image(source_img_path, target_video_path, model_path, output_video_path)
+    process_image_with_source_image(source_img_path, target_img_path, model_path, output_image_path)
 
-    print(f"Output video saved as: {output_video_path}")
+    print(f"Output image saved as: {output_image_path}")
 
 if __name__ == "__main__":
     main()
